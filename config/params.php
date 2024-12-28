75e984f2f4b62dda514342280aafe025bdfeaa7b
SassyAsh <swannmerienne@gmail.com>
2024-12-28 16:06:56 +0100

    index init

--- config/params.php
+++ config/params.php
@@ -1,3 +1,10 @@
 <?php
 
+define("CONFIG",    realpath(WORKDIR . "/config"));
+define("VUE",       realpath(WORKDIR . "/vue"));
+define("MODELE",    realpath(WORKDIR . "/modele"));
+define("CONTROLEUR",realpath(WORKDIR . "/controleur"));
+define("CSS",       realpath(WORKDIR . "/css"));
+define("ASSETS",    realpath(WORKDIR . "/assets"));
+
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
 define("WORKDIR", __DIR__);
-
 require_once(WORKDIR . '/config/params.php');
 
-echo "test";
+include_once(VUE . "/debut.php");
 
+echo "CWD : ". getcwd();
+
+echo "<ul>";
+echo "<li>Workdir : " . WORKDIR . "</li>";
+echo "<li>Config : " .  CONFIG . "</li>";
+echo "<li>Vue : " .     VUE . "</li>";
+echo "<li>Modele : " .  MODELE . "</li>";
+echo "<li>Controleur : ".CONTROLEUR . "</li>";
+echo "<li>CSS : " .     CSS . "</li>";
+echo "<li>Assets : " .  ASSETS . "</li>";
+echo "</ul>";
+
+include_once(VUE . "/fin.php");
 ?>
