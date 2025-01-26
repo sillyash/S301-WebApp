5d1edaad74cbfa2170a8942ecd17d4e12a908a7a
Ashley <merienne.ashley@gmail.com>
2025-01-26 17:40:38 +0100

    Refactor CSS structure: consolidate styles into app.css, remove unused files, and update package dependencies

--- vue/debut.php
+++ vue/debut.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>DemocHub / DemoCité</title>
-	<link rel="stylesheet" href="css/common.css"/>
+	<link rel="stylesheet" href="css/app.css"/>
 	<link rel="icon" type="image/x-icon" href="assets/favicon.svg"/>
 </head>
 <body>
