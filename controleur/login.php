c0b3d85c23352fda8d5dd881a95049b7440a8440
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-01-30 20:21:17 +0000

    feat : maybe css?

--- controleur/login.php
+++ controleur/login.php
@@ -2,8 +2,12 @@
 require_once(__DIR__ . "/../config/params.php");
 
 $createAccount = isset($_GET["create"]) ? true : false;
+$homePage = isset($_GET["home"]) ? true : false;
+
 if ($createAccount){
     include(VUE . "/createAccount.html");
+} else if ($homePage){
+    include(VUE . "/accueil.html");
 } else {
     include(VUE . "/login.html");
 }
