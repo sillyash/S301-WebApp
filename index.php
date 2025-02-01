25f4f6abe3f551e68b254b44d862896d951bffb0
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-01 17:26:11 +0000

    test : nav

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if ($_SESSION["logged"] == "true") {
     if (isset($_GET["propos"]) || isset($_GET["account"]) || isset($_GET["home"])) {
         include(CONTROLEUR . "/navigation.php");
     } else {
-        include(VUE . "/?home=true");
+        include(VUE . "/accueil.html");
     }
 } else {
     include(VUE . "/login.html");
