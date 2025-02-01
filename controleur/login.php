70bc46ceea5e95b61acf93dceb6f58bed9b3d6c1
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-01 15:35:03 +0000

    feat : css and more nav testing

--- controleur/login.php
+++ controleur/login.php
@@ -8,6 +8,7 @@ if ($createAccount){
     include(VUE . "/createAccount.html");
 } else if ($homePage){
     $_SESSION["logged"] = "true";
+    include(CONTROLEUR . "/navigation.php");
     include(VUE . "/accueil.html");
 } else {
     include(VUE . "/login.html");
--- index.php
+++ index.php
@@ -5,6 +5,5 @@ $_SESSION["logged"] = "true";
 
 include(VUE . "/debut.php");
 include(CONTROLEUR . "/login.php");
-include(CONTROLEUR . "/navigation.php");
 include(VUE . "/fin.html");
 ?>
