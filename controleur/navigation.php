7796eceafd5e13a7323dc7db3209a67af8ede075
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-01 16:54:20 +0000

    refactor : deleted useless stuff and more nav testing

--- controleur/navigation.php
+++ controleur/navigation.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . "/../config/params.php");
 
-$homePage = isset($_GET["home"]) ? true : false;
+$home = isset($_GET["home"]) ? true : false;
 $propos = isset($_GET["propos"]) ? true : false;
 $account = isset($_GET["account"]) ? true : false;
 
@@ -9,7 +9,7 @@ if ($propos){
     include(VUE . "/mesPropos.html");
 } else if ($account){
     include(VUE . "/monCompte.html");
-} else if ($homePage){
+} else if ($home){
     include(VUE . "/accueil.html");
 } else {
     include(VUE . "/login.html");
