cb39df2ee5eb6a5ea8f3f18164a1b5f4d939ee30
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 09:28:02 +0000

    feat : nav

--- controleur/navigation.php
+++ controleur/navigation.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . "/../config/params.php");
 
 $propos = isset($_GET["propos"]) ? true : false;
 $account = isset($_GET["account"]) ? true : false;
-
+require(VUE . "/debut.php");
 if ($propos){
     include(VUE . "/mesPropos.html");
 } else if ($account){
@@ -11,4 +11,5 @@ if ($propos){
 } else {
     include(VUE . "/accueil.html");
 }
+include(VUE . "/fin.html");
 ?>
