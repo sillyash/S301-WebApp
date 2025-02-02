933bde4a7c52ca2514e5d6cd0c44b8d9170b1c2d
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 11:44:42 +0000

    feat : added invite member page

--- controleur/navigation.php
+++ controleur/navigation.php
@@ -2,6 +2,7 @@
 require_once(__DIR__ . "/../config/params.php");
 
 $propos = isset($_GET["propos"]) ? true : false;
+$invite = isset($_GET["invite"]) ? true : false;
 $account = isset($_GET["account"]) ? true : false; // TODO get the following from DB instead of true/false (use ids)
 $ceGroupe = isset($_GET["ceGroupe"]) ? true : false;
 $membres = isset($_GET["membres"]) ? true : false;
@@ -15,6 +16,8 @@ if ($propos){
     include(VUE . "/ceGroupe.html");
 }else if ($membres){
     include(VUE . "/voirMembres.html");
+}else if ($invite){
+    include(VUE . "/inviterMembre.html");
 } else {
     include(VUE . "/accueil.html");
 }
