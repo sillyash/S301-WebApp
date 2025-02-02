3d18e7029646fa3fa3bb3169f4c938805cc1ae7a
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 09:20:55 +0000

    feat : nav

--- controleur/createPropo.php
+++ controleur/createPropo.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . "/../config/params.php");
 
 $createPropo = isset($_GET["createPropo"]) ? true : false;
 
-if ($createGroup){
+if ($createPropo){
     include(VUE . "/createPropo.html");
 }else {
     include(VUE . "/mesPropos.html");
