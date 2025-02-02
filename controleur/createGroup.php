d9a0aa2e81841f89a13010b46b4a0621e1b2f8b9
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 09:47:58 +0000

    feat : added admin logo and todos

--- controleur/createGroup.php
+++ controleur/createGroup.php
@@ -2,6 +2,8 @@
 require_once(__DIR__ . "/../config/params.php");
 
 $createGroup = isset($_GET["createGroup"]) ? true : false;
+// TODO : give internaute admin role upon group creation
+
 require(VUE . "/debut.php");
 if ($createGroup){
     include(VUE . "/createGroup.html");
