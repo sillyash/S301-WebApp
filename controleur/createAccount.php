79755886622e5be542c80562e1d818120b96eb1c
Ashley <merienne.ashley@gmail.com>
2025-02-02 21:08:55 +0100

    feat: initialize compteValide in account creation process

--- controleur/createAccount.php
+++ controleur/createAccount.php
@@ -20,6 +20,7 @@ require_once VUE . '/fin.php';
 function handleForm() : bool {
     try {
         Internaute::init();
+        $_POST["compteValide"] = false;
         $internaute = new Internaute($_POST, CONSTRUCT_POST);
         $data_json = json_encode($internaute);
     } catch (Exception $e) {
