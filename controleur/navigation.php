fb8cb159f3028bfa8ccc48dbb549b4589b898937
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 10:42:09 +0000

    feat : nav

--- controleur/navigation.php
+++ controleur/navigation.php
@@ -3,13 +3,13 @@ require_once(__DIR__ . "/../config/params.php");
 
 $propos = isset($_GET["propos"]) ? true : false;
 $account = isset($_GET["account"]) ? true : false;
-$account = isset($_GET["thisGroup"]) ? true : false;
+$account = isset($_GET["ceGroupe"]) ? true : false;
 require(VUE . "/debut.php");
 if ($propos){
     include(VUE . "/mesPropos.html");
 } else if ($account){
     include(VUE . "/monCompte.html");
-}else if ($thisGroup){
+}else if ($ceGroupe){
     include(VUE . "/ceGroupe.html");
 } else {
     include(VUE . "/accueil.html");
