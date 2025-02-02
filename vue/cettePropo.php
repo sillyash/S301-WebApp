3c9c73ac169acc6f42d774126a59be0903cfdb99
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 19:57:35 +0000

    feat : added specific propo page & associated nav

---
+++ vue/cettePropo.php
@@ -0,0 +1,25 @@
+<div class="pageTitleBar">
+    CETTE PROPOSITION
+</div>
+<!-- TODO retrieve data from BDD -->
+<!-- TODO change form action -->
+<!-- TODO handle reactions here -->
+<div>
+    <p>[TITRE]</p>
+    <p>[THEMES]</p>
+    <hr>
+    <p>Description</p>
+</div>
+<!-- TODO maybe this should be a seperate component? -->
+<div>
+    <p>ESPACE COMMENTAIRES</p>
+    <form>
+        <div class="formRow">
+            <label class="flex-1" for="loginInter">Login*</label>
+            <input class="flex-2" type="text" name="loginInter" required>
+        </div>
+        <button class="!bg-white !text-blue-500 hover:!text-blue-600" type="submit">
+            Ajouter un commentaire
+        </button>
+    </form>
+</div>
--- vue/mesPropos.php
+++ vue/mesPropos.php
@@ -9,19 +9,19 @@
         <li class="w-full">
             <div class="flex items-center justify-between w-full">
                 <div>PROPO 1</div>
-                <a href="index.php">➜</a>
+                <a href= "<?php echo ROOT_URL .'/controleur/cettePropo.php'?>">➜</a>
             </div>
         </li>
         <li class="w-full">
             <div class="flex items-center justify-between w-full">
                 <div>PROPO 2</div>
-                <a href="index.php">➜</a>
+                <a href= "<?php echo ROOT_URL .'/controleur/cettePropo.php'?>">➜</a>
             </div>
         </li>
         <li class="w-full">
             <div class="flex items-center justify-between w-full">
                 <div>PROPO 3</div>
-                <a href="index.php">➜</a>
+                <a href= "<?php echo ROOT_URL .'/controleur/cettePropo.php'?>">➜</a>
             </div>
         </li>
     </ul>
