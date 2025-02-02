bd86120e0efd76672d4754b38fe941a30c0aa529
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 11:31:38 +0000

    feat : added page to see a group's members

--- controleur/navigation.php
+++ controleur/navigation.php
@@ -2,14 +2,18 @@
 require_once(__DIR__ . "/../config/params.php");
 
 $propos = isset($_GET["propos"]) ? true : false;
-$account = isset($_GET["account"]) ? true : false;
+$account = isset($_GET["account"]) ? true : false; // TODO get the following from DB instead of true/false (use ids)
 $ceGroupe = isset($_GET["ceGroupe"]) ? true : false;
+$membres = isset($_GET["membres"]) ? true : false;
+
 require(VUE . "/debut.php");
 if ($propos){
     include(VUE . "/mesPropos.html");
 } else if ($account){
     include(VUE . "/monCompte.html");
 }else if ($ceGroupe){
+    include(VUE . "/ceGroupe.html");
+}else if ($membres){
     include(VUE . "/ceGroupe.html");
 } else {
     include(VUE . "/accueil.html");
