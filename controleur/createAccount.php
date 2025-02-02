a1885317a7f5b181d363284e39504bed48845308
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-02 18:14:36 +0000

    test : nav

--- controleur/createAccount.php
+++ controleur/createAccount.php
@@ -44,6 +44,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             } else {
                 echo "<div class='error'><p>Error: $sqlError</p></div>";
             }
+        } else {
+            include(VUE . "/accueil.php");
         }
     } catch (Exception $e) {
         echo "<div class='error'><p>Error: " . $e->getMessage(). "</p></div>";
