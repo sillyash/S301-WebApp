491f8dddc6d6155db9b65b48074518ac84856b48
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-04 20:29:00 +0000

    feat : display monCompte

--- controleur/monCompte.php
+++ controleur/monCompte.php
@@ -1,8 +1,42 @@
 <?php
 require_once("../config/params.php");
-require(VUE . "/debut.php");
+include(VUE . "/debut.php");
 
+if (isset($_SESSION["logged"])) {
+    if ($_SESSION["logged"] != true) {
+        header("Location: " . ROOT_URL);
+    }
+} else {
+    header("Location: " . ROOT_URL);
+}
+
+$userData = getUtilisateur();
 include(VUE . "/monCompte.php");
 
 require(VUE . "/fin.php");
+
+function getUtilisateur() : array {
+    try {
+        $handle = curl_init();
+        $url = API_URL . "view/Internaute?loginInter=" . $_SESSION["login"];
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        if (!$response) throw new Exception("Response is empty/false");
+        else {
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            if ($httpCode > 299 || $httpCode < 200) {
+                $response = json_decode($response, true);
+                $sqlError = $response['error'];
+                echo "<div class='error'><p>Error: $sqlError</p></div>";
+            }
+        }
+        return json_decode($response, true);
+    } catch (Throwable $e) {
+        echo "<div class='error'>";
+        echo "<p>Error executing GET request : " . $e->getMessage() . "<p></div>";
+    }
+}
 ?>
--- vue/monCompte.php
+++ vue/monCompte.php
@@ -5,35 +5,25 @@
 <!-- TODO: for loop to retrieve data for a user -->
 <!-- TODO: opt - add edit field possibility -->
 <div class="flex flex-col justify-space-between align-center w-200">
-    <div>
-        <p class="flex-1">Login:</p>
-        <p> [login] </p>
-    </div>
-    
-    <div>
-        <p class="flex-1">Mot de passe:</p>
-        <p> [mdp] </p>
-    </div>
 
-    <div>
-        <p class="flex-1">Adresse Mail:</p>
-        <p> [email] </p>
-    </div>
-
-    <div>
-        <p class="flex-1">Nom:</p>
-        <p> [nom] </p>
-    </div>
-    
-    <div>
-        <p class="flex-1">Prénom:</p>
-        <p> [prenom] </p>
-    </div>
-
-    <div>
-        <p class="flex-1">Adresse Postale:</p>
-        <p> [adr] </p>
-    </div>
+<?php
+foreach ($userData as $data) {
+    $login = $data["loginInter"];
+    $mdp = $data["idGroupe"];
+    $email = $data["emailInter"];
+    $nom = $data["nomInter"];
+    $prenom = $data["prenomInter"];
+    $addr = $data["adrInter"];
+    echo "<div class='flex flex-row align-middle bg-blue-500 rounded-md mx-1 my-2 p-2 justify-end h-20 items-center'>";
+    echo "<span class='flex-2 text-lg mx-4'>$login</span>";
+    echo "<span class='flex-2 text-lg mx-4'>$mdp</span>";
+    echo "<span class='flex-2 text-lg mx-4'>$email</span>";
+    echo "<span class='flex-2 text-lg mx-4'>$nom</span>";
+    echo "<span class='flex-2 text-lg mx-4'>$prenom</span>";
+    echo "<span class='flex-2 text-lg mx-4'>$addr</span>";
+    echo "</div></a>";
+}
+?>
 
     <!-- TODO: change action of each button -->
     <a>
