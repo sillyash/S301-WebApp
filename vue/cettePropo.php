43588c3ec0bb2a5d90885489ba61666a84aa2eba
Ashley <merienne.ashley@gmail.com>
2025-02-04 22:11:37 +0100

    feat: implement proposition detail retrieval and display

--- vue/cettePropo.php
+++ vue/cettePropo.php
@@ -1,24 +1,14 @@
-<div class="pageTitleBar">
-    CETTE PROPOSITION [TITRE]
-</div>
-<!-- TODO retrieve data from BDD -->
-<!-- TODO change form action -->
-<!-- TODO handle reactions here -->
-<div class="bg-white rounded-md w-1/2 p-4">
-    <p>[THEMES]</p>
-    <hr>
-    <p>Description</p>
-</div>
-<!-- TODO maybe this should be a seperate component? -->
-<div class="w-1/2 p-4">
-    <p>ESPACE COMMENTAIRES</p>
-    <form>
-        <div>
-            <label class="flex-1" for="comment">[USERNAME]</label>
-            <input class="flex-2" type="text" name="comment" required>
-        </div>
-        <button class="!bg-white !text-blue-500 hover:!text-blue-600" type="submit">
-            Ajouter un commentaire
-        </button>
-    </form>
-</div>
+<?php
+
+$titre = $proposition['titreProposition'];
+$desc = $proposition['descriptionProposition'];
+$theme = $proposition['theme'];
+$popularity = $proposition['popularite'];
+$groupe = $proposition['groupe'];
+$cout = $proposition['cout'];
+
+echo "<div class='pageTitleBar'>$titre</div>";
+echo "<div class='bg-white rounded-md w-1/2 p-4'>";
+echo "<p>$theme</p><hr><p>$desc</p></div>";
+
+?>
