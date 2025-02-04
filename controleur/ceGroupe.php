92d2a4d831e8505c5b58437c23fda80884f7a2bc
Ashley <merienne.ashley@gmail.com>
2025-02-04 18:34:15 +0100

    feat: implement group details retrieval with error handling and display in view

--- controleur/ceGroupe.php
+++ controleur/ceGroupe.php
@@ -1,10 +1,105 @@
 <?php
 require_once("../config/params.php");
+require_once(MODELE . "/Groupe.php");
 require(VUE . "/debut.php");
 
-// TODO: BDD call to retrieve user data
+if (!isset($_GET['groupe'])) {
+    echo "<div class='error'><p>Groupe non spécifié</p>";
+    echo "<p>Veuillez revenir a la page d'accueil.</p></div>";
+    require(VUE . "/fin.php");
+    exit();
+}
+
+$idGroupe = $_GET['groupe'];
+$groupe = apiGetGroupe($idGroupe)[0];
+$votes = apiGetVotes($idGroupe);
+$propositions = apiGetPropositions($idGroupe);
 
 include(VUE . "/ceGroupe.php");
 
 require(VUE . "/fin.php");
+
+/* ------------------ Functions ------------------ */
+
+function apiGetGroupe($idGroupe) {
+    try {
+        $handle = curl_init();
+        $url = API_URL . "Groupe?idGroupe=$idGroupe";
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        if (!$response) throw new Exception("Response is empty/false. URL = $url");
+        else {
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            if ($httpCode > 299 || $httpCode < 200) {
+                $response = json_decode($response, true);
+                $sqlError = $response['error'];
+                echo "<div class='error'><p>Error: $sqlError</p></div>";
+            }
+        }
+        return json_decode($response, true);
+    } catch (Throwable $e) {
+        echo "<div class='error'>";
+        echo "<p>Error executing GET request : " . $e->getMessage() . "<p></div>";
+    }
+
+    return json_decode($response);
+}
+
+function apiGetVotes($idGroupe) {
+    try {
+        $handle = curl_init();
+        $url = API_URL . "view/VotesGroupe?idGroupe=$idGroupe";
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        if (!$response) throw new Exception("Response is empty/false. URL = $url");
+        else {
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            if ($httpCode > 299 || $httpCode < 200) {
+                $response = json_decode($response, true);
+                $sqlError = $response['error'];
+                echo "<div class='error'><p>Error: $sqlError</p></div>";
+            }
+        }
+        return json_decode($response, true);
+    } catch (Throwable $e) {
+        echo "<div class='error'>";
+        echo "<p>Error executing GET request : " . $e->getMessage() . "<p></div>";
+    }
+
+    return json_decode($response);
+}
+
+function apiGetPropositions($idGroupe) {
+    try {
+        $handle = curl_init();
+        $url = API_URL . "view/PropositionsGroupe?idGroupe=$idGroupe";
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        if (!$response) throw new Exception("Response is empty/false. URL = $url");
+        else {
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            if ($httpCode > 299 || $httpCode < 200) {
+                $response = json_decode($response, true);
+                $sqlError = $response['error'];
+                echo "<div class='error'><p>Error: $sqlError</p></div>";
+            }
+        }
+        return json_decode($response, true);
+    } catch (Throwable $e) {
+        echo "<div class='error'>";
+        echo "<p>Error executing GET request : " . $e->getMessage() . "<p></div>";
+    }
+
+    return json_decode($response);
+}
+
 ?>
--- vue/ceGroupe.php
+++ vue/ceGroupe.php
@@ -1,65 +1,46 @@
-<div class="pageTitleBar">
-    CE GROUPE
-</div>
+<?php
+
+$image = $groupe['imgGroupe'] ?? "https://picsum.photos/200/200";
+$desc = $groupe['descGroupe'];
+$nom = $groupe['nomGroupe'];
+
+echo "<div class='flex items-center justify-around w-full p-5 my-5'>";
+echo "<img src='$image' alt='groupe' class='mx-20 rounded-full w-30 h-30 object-contain'>";
+echo "<p class='mx-5 text-xl underline h-full align-middle text-left text-black p-5'>$nom</p>";
+echo "<div class='mx-10 w-100 h-full align-middle justify-center bg-gray-500 rounded-md text-white p-5'>";
+echo "$desc</div></div>";
+
+echo "<div class='flex space-x-4'><div class='bg-blue-500 text-white rounded-md p-4 m-4'>";
+echo "<p><strong>VOTES EN COURS</strong></p><hr><ul class='w-full'>";
+
+foreach ($votes as $vote) {
+    $idVote = $vote['idVote'];
+    $titre = $vote['titreVote'];
+    $pour = $vote['pour'];
+    $contre = $vote['contre'];
+
+    echo "<li class='w-full'><div class='flex flex-col items-start w-full'>";
+    echo "<div>$titre</div><div>POUR: $pour | CONTRE: $contre</div></div></li>";
+}
+
+echo "</ul></div>";
+echo "<div class='flex space-x-4'><div class='bg-blue-500 text-white rounded-md p-4 m-4'>";
+echo "<p><strong>VOTES EN COURS</strong></p><hr><ul class='w-full'>";
+
+foreach ($propositions as $proposition) {
+    $idProposition = $proposition['idProposition'];
+    $titre = $proposition['titreProposition'];
+
+    echo "<li class='w-full'><div class='flex items-center justify-between w-full'>";
+    echo "<div>$titre</div>";
+    echo "<a href='".ROOT_URL."controleur/lancerVote.php'>";
+    echo "<button class='m-4 !bg-white !text-red-500 hover:!text-orange-500'>Déclencher vote</button></a></div></li>";
+}
+
+echo "</ul></div></div>";
+
+?>
+
 <a href="<?php echo ROOT_URL.'controleur/voirMembres.php'?>">
     <button class="m-4">Voir membres</button>
 </a>
-
-<!-- TODO add get nb votes from DB -->
-<div class="flex space-x-4">
-    <div class="bg-blue-500 text-white rounded-md p-4 m-4">
-        <p><strong>VOTES EN COURS</strong></p>
-        <hr>
-        <ul class="w-full">
-            <li class="w-full">
-                <div class="flex flex-col items-start w-full">
-                    <div>Vote 1</div>
-                    <div>POUR: [nb] | CONTRE: [nb]</div>
-                </div>
-            </li>
-            <li class="w-full">
-                <div class="flex flex-col items-start w-full">
-                    <div>Vote 2</div>
-                    <div>POUR: [nb] | CONTRE: [nb]</div>
-                </div>
-            </li>
-            <li class="w-full">
-                <div class="flex flex-col items-start w-full">
-                    <div>Vote 3</div>
-                    <div>POUR: [nb] | CONTRE: [nb]</div>
-                </div>
-            </li>
-        </ul>
-    </div>    
-
-    <div class="bg-blue-500 text-white rounded-md p-4 m-4">
-        <p><strong>LISTE PROPOSITIONS</strong></p>
-        <hr>
-        <ul class="w-full">
-            <li class="w-full">
-                <div class="flex items-center justify-between w-full">
-                    <div>PROPO 1</div>
-                    <a href="<?php echo ROOT_URL.'controleur/lancerVote.php'?>">
-                        <button class="m-4 !bg-white !text-red-500 hover:!text-orange-500">Déclencher vote</button>
-                    </a>
-                </div>
-            </li>
-            <li class="w-full">
-                <div class="flex items-center justify-between w-full">
-                    <div>PROPO 2</div>
-                    <a href="<?php echo ROOT_URL.'controleur/lancerVote.php'?>">
-                        <button class="m-4 !bg-white !text-red-500 hover:!text-orange-500">Déclencher vote</button>
-                    </a>
-                </div>
-            </li>
-            <li class="w-full">
-                <div class="flex items-center justify-between w-full">
-                    <div>PROPO 3</div>
-                    <a href="<?php echo ROOT_URL.'controleur/lancerVote.php'?>">
-                        <button class="m-4 !bg-white !text-red-500 hover:!text-orange-500">Déclencher vote</button>
-                    </a>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div>
