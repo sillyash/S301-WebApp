920d0aa6b93e629a1bf1901d43704fc065345a99
Ashley <merienne.ashley@gmail.com>
2025-02-04 13:37:35 +0100

    feat : code pwetty

--- controleur/createAccount.php
+++ controleur/createAccount.php
@@ -20,6 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 require(VUE . '/fin.php');
 
+/* --------------- Functions --------------- */
+
 function handleForm() : bool {
     try {
         Internaute::init();
