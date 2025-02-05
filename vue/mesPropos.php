c358196245aae4d6796c73377ddeb2284ceafb19
Ashley <merienne.ashley@gmail.com>
2025-02-05 23:50:25 +0100

    feat : enhance proposal display with improved layout and navigation

--- vue/mesPropos.php
+++ vue/mesPropos.php
@@ -2,18 +2,22 @@
 <div class="pageTitleBar">
     Mes Propositions
 </div>
-<div class="bg-blue-500 rounded-md">
-    <?php
-    foreach ($propositions as $proposition) {
-        $idProposition = $proposition['idProposition'];
-        $titreProposition = $proposition['titreProposition'];
-        $descProposition = $proposition['descProposition'];
+<div class="flex flex-col justify-center align-middle w-full my-4 mx-2 p-1">
 
-        echo "<a class='!bg-white !text-red-500 hover:!text-orange-500 rounded-md' ";
-        echo "href='" . ROOT_URL . "controleur/cettePropo.php?idProposition=$idProposition'>";
-        echo "<li class='flex items-center justify-between w-full p-2 m-2'>$titreProposition : $descProposition";
-        echo "</a></li>";
-    }
-    ?>
+<?php
+foreach ($propositions as $proposition) {
+    $idProposition = $proposition['idProposition'];
+    $titreProposition = $proposition['titreProposition'];
+    $descProposition = $proposition['descProposition'];
+    $href = ROOT_URL . "controleur/cettePropo.php?idProposition=$idProposition'";
+
+    echo "<a class='text-white hover:text-red-500' href='$href'>";
+    echo "<div class='flex flex-row align-middle bg-blue-500 rounded-md mx-1 my-2 p-2 justify-end h-20 items-center'>";
+    echo "<span class='flex-2 text-lg mx-4'>$titreProposition : $descProposition</span>";
+    echo "<span class='flex-2 text-right text-2xl mx-4'>➜</span>";
+    echo "</div></a>";
+}
+?>
+
 </div>
 
