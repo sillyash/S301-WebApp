64fba9578213d1d830d1066c8abe8cf438f9d51b
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-05 21:26:00 +0000

    feat : maybe it work now

--- vue/voirMembres.php
+++ vue/voirMembres.php
@@ -10,6 +10,13 @@
             $role = $membre['role'];
             echo "<li class='flex items-center justify-between w-full p-2 m-2'>";
             echo "$login : $role";
+            echo "<a href='" . ROOT_URL . "controleur/modifierRole.php?idMembre=$idMembre'>
+                <button class='m-4 !bg-white !text-red-500 hover:!text-orange-500'>Modifier Rôle</button>
+                </a>";
+            echo "<a href='" . ROOT_URL . "controleur/deleteThisUser.php?idMembre=$idMembre'>
+                <button class='m-4 !bg-white !text-red-500 hover:!text-orange-500'>Supprimer</button>
+                </a>";
+
             echo "href='".ROOT_URL."controleur/modifierRole.php?idMembre=$login'>";
             echo "href='".ROOT_URL."controleur/deleteThisUser.php?idMembre=$login'>";
             echo "</li>";
