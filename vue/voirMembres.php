df1893d789d0b688c8833cffb20dad769c1f9ed4
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-05 21:22:38 +0000

    test

--- vue/voirMembres.php
+++ vue/voirMembres.php
@@ -8,16 +8,14 @@
         foreach ($membres as $membre): 
             $login = $membre['loginInter'];
             $role = $membre['role'];
-            $idMembre = $membre['id'];
             echo "<li class='flex items-center justify-between w-full p-2 m-2'>";
             echo "$login : $role";
-            echo "href='".ROOT_URL."controleur/modifierRole.php?idMembre=$idMembre'>";
-            echo "href='".ROOT_URL."controleur/deleteThisUser.php?idMembre=$idMembre'>";
+            echo "href='".ROOT_URL."controleur/modifierRole.php?idMembre=$login'>";
+            echo "href='".ROOT_URL."controleur/deleteThisUser.php?idMembre=$login'>";
             echo "</li>";
         endforeach;
         ?>
     </ul>
-    echo "href='".ROOT_URL."controleur/deleteThisUser.php?idMembre=$idMembre'>";
     <a href="<?php echo ROOT_URL . 'controleur/inviterMembre.php'; ?>">
         <button class="w-full">Inviter un nouveau membre</button>
     </a>
