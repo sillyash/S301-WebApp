9f8c8bdef1224965f6c0c09e9ebf1a932d5d4fc4
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-05 20:29:44 +0000

    feat : new mes propos view

--- controleur/mesPropos.php
+++ controleur/mesPropos.php
@@ -1,10 +1,38 @@
 <?php
 require_once("../config/params.php");
 require(VUE . "/debut.php");
-
-// TODO: BDD call to retrieve user data
+$login = $_SESSION["login"];
+$propositions = apiGetPropositions($login);
 
 include(VUE . "/mesPropos.php");
 
 require(VUE . "/fin.php");
+
+/* ------------------ Functions ------------------ */
+function apiGetPropositions($login) {
+    try {
+        $handle = curl_init();
+        $url = API_URL . "view/PropositionsUtilisateur?loginInter=$login";
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        if (!$response) throw new Exception("Response is empty/false. URL = $url");
+        else {
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            if ($httpCode > 299 || $httpCode < 200) {
+                $response = json_decode($response, true);
+                $sqlError = isset($response["error"]) ? $response["error"] : $response;
+                echo "<div class='error'><p>Error: $sqlError</p></div>";
+            }
+        }
+        return json_decode($response, true);
+    } catch (Throwable $e) {
+        echo "<div class='error'>";
+        echo "<p>Error executing GET request : " . $e->getMessage() . "<p></div>";
+    }
+
+    return json_decode($response);
+}
 ?>
--- vue/mesPropos.php
+++ vue/mesPropos.php
@@ -25,5 +25,17 @@
             </div>
         </li>
     </ul>
+    <?php
+    foreach ($propositions as $proposition) {
+        $idProposition = $proposition['idProposition'];
+        $titreProposition = $proposition['titreProposition'];
+        $descProposition = $proposition['descProposition'];
+
+        echo "<a class='!bg-white !text-red-500 hover:!text-orange-500 rounded-md' ";
+        echo "href='" . ROOT_URL . "controleur/cettePropo.php?idProposition=$idProposition'>";
+        echo "<li class='flex items-center justify-between w-full p-2 m-2'>$titreProposition : $descProposition";
+        echo "</a></li>";
+    }
+    ?>
 </div>
 
