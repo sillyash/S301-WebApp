5de7ebb157ea421758281325fcb2c9a0cba5888b
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-05 21:19:06 +0000

    feat : fixed voirMembres maybe

--- vue/voirMembres.php
+++ vue/voirMembres.php
@@ -4,28 +4,21 @@
 
 <div>
     <ul class="w-full">
-        <?php if (!empty($membres) && is_array($membres)): ?>
-            <?php foreach ($membres as $membre): ?>
-                <li class="w-full">
-                    <div class="flex items-center justify-between w-full">
-                        <div><?php echo htmlspecialchars($membre['login']); ?></div>
-                        <div>[ <?php echo htmlspecialchars($membre['role']); ?> ]</div>
-                        <a href="<?php echo ROOT_URL.'controleur/modifierRole.php?idMembre='.urlencode($membre['id']); ?>">
-                            <button class="m-4 !bg-white !text-red-500 hover:!text-orange-500">Changer rôle</button>
-                        </a>
-                        <a href="<?php echo ROOT_URL.'controleur/supprimerMembre.php?idMembre='.urlencode($membre['id']); ?>">
-                            <button class="m-4 !bg-white !text-red-500 hover:!text-orange-500">Supprimer</button>
-                        </a>
-                    </div>
-                </li>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <li class="w-full">
-                <div class="text-center">Aucun membre trouvé.</div>
-            </li>
-        <?php endif; ?>
+        <?php 
+        foreach ($membres as $membre): 
+            $login = $membre['login'];
+            $role = $membre['role'];
+            $idMembre = $membre['id'];
+            echo "<li class='flex items-center justify-between w-full p-2 m-2'>";
+            echo "$login : $role";
+            echo "href='".ROOT_URL."controleur/modifierRole.php?idMembre=$idMembre'>";
+            echo "href='".ROOT_URL."controleur/deleteThisUser.php?idMembre=$idMembre'>";
+            echo "</li>";
+        endforeach;
+        ?>
     </ul>
-    <a href="<?php echo ROOT_URL.'controleur/inviterMembre.php?groupe='.urlencode($idGroupe); ?>">
+    echo "href='".ROOT_URL."controleur/deleteThisUser.php?idMembre=$idMembre'>";
+    <a href="<?php echo ROOT_URL . 'controleur/inviterMembre.php'; ?>">
         <button class="w-full">Inviter un nouveau membre</button>
     </a>
 </div>
