7bc6aa84a7da1bf44a7f31ee75247b7502a9b855
SassyAsh <swannmerienne@gmail.com>
2025-02-05 17:04:39 +0100

    refactor: improve HTML structure and class ordering in ceGroupe view

--- vue/ceGroupe.php
+++ vue/ceGroupe.php
@@ -14,21 +14,21 @@ $nom = $groupe['nomGroupe'];
 $color = $groupe['couleurGroupe'];
 $contrastColor = getContrastColor($color);
 
-echo "<div class='flex items-center justify-center w-full h-15 p-2 my-5 mx-5'>";
-echo "<a class='flex-1 align-middle justify-center h-full w-full text-center rounded-md p-2 mx-5 bg-blue-500 text-white' ";
+echo "<div class='flex items-center justify-center w-full p-2 mx-5 my-5 h-15'>";
+echo "<a class='justify-center flex-1 w-full h-full p-2 mx-5 text-center text-white align-middle bg-blue-500 rounded-md' ";
 echo "href='". ROOT_URL. "controleur/voirMembres.php'>Voir membres</a>";
-echo "<p class='flex-1 align-middle justify-center h-full w-full text-center rounded-md p-2 mx-5' ";
+echo "<p class='justify-center flex-1 w-full h-full p-2 mx-5 text-center align-middle rounded-md' ";
 echo "style='color:$color;background-color:$contrastColor;'>Votre role : $role</p>";
 echo "</div>";
 
 echo "<div class='flex items-center justify-around w-full p-5 my-5'>";
-echo "<img src='$image' alt='groupe' class='mx-20 rounded-full w-30 h-30 object-contain'>";
-echo "<p class='mx-5 text-2xl font-bold underline h-full align-middle text-left p-5' style='color:$color;'>$nom</p>";
-echo "<div class='mx-10 w-100 h-full align-middle justify-center rounded-md p-5'";
+echo "<img src='$image' alt='groupe' class='object-contain mx-20 rounded-full w-30 h-30'>";
+echo "<p class='h-full p-5 mx-5 text-2xl font-bold text-left underline align-middle' style='color:$color;'>$nom</p>";
+echo "<div class='justify-center h-full p-5 mx-10 align-middle rounded-md w-100'";
 echo "style='color:$contrastColor;background-color:$color;'>";
 echo "$desc</div></div>";
 
-echo "<div class='flex flex-1'><div class='bg-blue-500 text-white rounded-md p-4 m-4'>";
+echo "<div class='flex flex-1'><div class='p-4 m-4 text-white bg-blue-500 rounded-md'>";
 echo "<p><strong>VOTES EN COURS</strong></p><hr><ul class='w-full'>";
 
 foreach ($scrutins as $scrutin) {
@@ -45,7 +45,7 @@ foreach ($scrutins as $scrutin) {
 }
 
 echo "</ul></div>";
-echo "<div class='flex flex-1'><div class='bg-blue-500 text-white rounded-md p-4 m-4'>";
+echo "<div class='flex flex-1'><div class='p-4 m-4 text-white bg-blue-500 rounded-md'>";
 echo "<p><strong>PROPOSITIONS</strong></p><hr><ul class='w-full'>";
 
 foreach ($propositions as $proposition) {
@@ -60,21 +60,21 @@ foreach ($propositions as $proposition) {
 
 echo "</ul></div></div></div>";
 
-echo "<div class='flex w-full justify-center align-center'>";
+echo "<div class='flex justify-center w-full align-center'>";
 echo "<a class='p-2 m-2 w-100' ";
 echo "href='" . ROOT_URL . "controleur/createPropo.php?idGroupe=$idGroupe'>";
 echo "<button class='w-full'>Créer une proposition</button></a></div>";
 
 if ($isAdmin) {
-    echo "<div class='flex w-full justify-center align-center'>";
+    echo "<div class='flex justify-center w-full align-center'>";
     echo "<a class='p-2 m-2 w-100' ";
     echo "href='" . ROOT_URL . "controleur/createBudget.php?idGroupe=$idGroupe'>";
     echo "<button class='w-full'>Créer un budget</button></a></div>";
 
-    echo "<div class='flex w-full justify-center align-center'>";
+    echo "<div class='flex justify-center w-full align-center'>";
     echo "<a class='p-2 m-2 w-100' ";
     echo "href='" . ROOT_URL . "controleur/createTheme.php?idGroupe=$idGroupe'>";
-    echo "<button class='w-full'>Ajouter des roles</button></a></div>";
+    echo "<button class='w-full'>Ajouter des thèmes</button></a></div>";
 }
 
 ?>
