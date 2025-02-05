9d505a57a7c36bc043d9bfd024d3d052e33ae5c3
nesperrr <114148411+nesperrr@users.noreply.github.com>
2025-02-05 20:32:23 +0000

    feat : css + cleanup

--- vue/mesPropos.php
+++ vue/mesPropos.php
@@ -2,29 +2,7 @@
 <div class="pageTitleBar">
     Mes Propositions
 </div>
-<div>
-    <!-- TODO add for loop to get all propos an internaute has done -->
-    <!-- TODO if there are no propos, display "Pas de propositions!" or smt -->
-    <ul class="w-full">
-        <li class="w-full">
-            <div class="flex items-center justify-between w-full">
-                <div>PROPO 1</div>
-                <a href= "<?php echo ROOT_URL.'controleur/cettePropo.php'?>">➜</a>
-            </div>
-        </li>
-        <li class="w-full">
-            <div class="flex items-center justify-between w-full">
-                <div>PROPO 2</div>
-                <a href= "<?php echo ROOT_URL.'controleur/cettePropo.php'?>">➜</a>
-            </div>
-        </li>
-        <li class="w-full">
-            <div class="flex items-center justify-between w-full">
-                <div>PROPO 3</div>
-                <a href= "<?php echo ROOT_URL.'controleur/cettePropo.php'?>">➜</a>
-            </div>
-        </li>
-    </ul>
+<div class="bg-blue-500 rounded-md">
     <?php
     foreach ($propositions as $proposition) {
         $idProposition = $proposition['idProposition'];
