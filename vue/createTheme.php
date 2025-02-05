e7cbb2567f67bcdae90e0f4375e373ea239ea995
SassyAsh <swannmerienne@gmail.com>
2025-02-05 17:35:57 +0100

    feat: refactor theme creation form for improved structure and functionality

--- vue/createTheme.php
+++ vue/createTheme.php
@@ -4,40 +4,41 @@
 <form class="flex flex-col justify-space-between align-center w-200" method="post" action="<?php echo ROOT_URL."controleur/createTheme.php?idGroupe=$idGroupe"; ?>">
 
 <div id="themesContainer">
-        <div class="formRow themeRow">
-            <label class="flex-1 my-2" for="themesGroupe[]">Nom*</label>
-            <input class="flex-2" type="text" name="themesGroupe[]" required>
-            <br>
-            <label class="flex-1 my-2" for="budgetsGroupe[]">Budget*</label>
-            <input class="flex-2" type="number" name="budgetsGroupe[]" required>
-            <button type="button" class="flex-1 mx-5 removeThemeBtn" onclick="removeTheme(this)">Supprimer</button>
-        </div>
+    <div class="formRow themeRow">
+        <label class="flex-1 my-2" for="themesGroupe[]">Nom*</label>
+        <input class="flex-2" type="text" name="themesGroupe[]" required>
+        <br>
+        <label class="flex-1 my-2" for="budgetsGroupe[]">Budget*</label>
+        <input class="flex-2" type="number" name="budgetsGroupe[]" required>
+        <button type="button" class="flex-1 mx-5 removeThemeBtn" onclick="removeTheme(this)">Supprimer</button>
     </div>
-    <button type="button" id="addThemeBtn" class="w-200! my-5!" onclick="addTheme()">Ajouter un theme</button>
-    
-    <script>
-        function addTheme() {
-            const container = document.getElementById('themesContainer');
-            const newFormRow = document.createElement('div');
-            newFormRow.className = 'formRow themeRow';
-            newFormRow.innerHTML = `
-            <label class="flex-1 my-2" for="themesGroupe[]">Nom*</label>
-            <input class="flex-2" type="text" name="themesGroupe[]" required>
-            <br>
-            <label class="flex-1 my-2" for="budgetsGroupe[]">Budget*</label>
-            <input class="flex-2" type="number" name="budgetsGroupe[]" required>
-            <button type="button" class="flex-1 mx-5 removeThemeBtn" onclick="removeTheme(this)">Supprimer</button>
-            `;
-            container.appendChild(newFormRow);
-        }
-        
-        function removeTheme(button) {
-            const row = button.parentElement;
-            row.remove();
-        }
-    </script>
+</div>
 
-    <button type="submit">
-        ➜
-    </button>
+<button type="button" id="addThemeBtn" class="w-200! my-5!" onclick="addTheme()">Ajouter un theme</button>
+
+<script>
+    function addTheme() {
+        const container = document.getElementById('themesContainer');
+        const newFormRow = document.createElement('div');
+        newFormRow.className = 'formRow themeRow';
+        newFormRow.innerHTML = `
+        <label class="flex-1 my-2" for="themesGroupe[]">Nom*</label>
+        <input class="flex-2" type="text" name="themesGroupe[]" required>
+        <br>
+        <label class="flex-1 my-2" for="budgetsGroupe[]">Budget*</label>
+        <input class="flex-2" type="number" name="budgetsGroupe[]" required>
+        <button type="button" class="flex-1 mx-5 removeThemeBtn" onclick="removeTheme(this)">Supprimer</button>
+        `;
+        container.appendChild(newFormRow);
+    }
+    
+    function removeTheme(button) {
+        const row = button.parentElement;
+        row.remove();
+    }
+</script>
+
+<button type="submit">
+    ➜
+</button>
 </form>
