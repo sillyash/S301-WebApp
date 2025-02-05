5ac787132cc35e0faf14922d27f902a540ee529c
Ashley <merienne.ashley@gmail.com>
2025-02-06 00:37:47 +0100

    fix: remove success message and delay from account deletion process

--- controleur/deleteAccount.php
+++ controleur/deleteAccount.php
@@ -44,9 +44,6 @@ if (ini_get("session.use_cookies")) {
     setcookie(session_name(), '', time() - 42000, '/');
 }
 
-echo "<p class='my-5'>Suppression du compte reussie.";
-echo "Redirection vers la page d'accueil...</p>";
-sleep(3);
 header("Location: login.php");
 
 ?>
