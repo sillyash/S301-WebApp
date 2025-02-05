2f9375b9a8e0c3a0bec509ad504aeab3111c241c
SassyAsh <swannmerienne@gmail.com>
2025-02-05 13:43:47 +0100

    feat: enhance proposal creation form with budget selection and additional fields

--- controleur/createPropo.php
+++ controleur/createPropo.php
@@ -2,7 +2,46 @@
 require_once(__DIR__ . "/../config/params.php");
 require(VUE . "/debut.php");
 
+if (!isset($_GET['idGroupe'])) {
+    echo "<div class='error'>Erreur : idGroupe non défini</div>";
+    require_once VUE . '/fin.php';
+    exit();
+}
+
+$idProposition = $_GET['idGroupe'];
+$budgets = apiGetBudgets($idGroupe);
+
 include(VUE . "/createPropo.php");
 
 require_once VUE . '/fin.php';
+
+/* --------------- Functions --------------- */
+
+function apiGetBudgets($idGroupe) {
+    try {
+        $handle = curl_init();
+        $url = API_URL . "BudgetsGroupe?idGroupe=" . $idGroupe;
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($handle);
+
+        if (!$response) throw new Exception("Response is empty/false. URL = $url");
+        else {
+            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            if ($httpCode > 299 || $httpCode < 200) {
+                $response = json_decode($response, true);
+                $sqlError = isset($response["error"]) ? $response["error"] : $response;
+                echo "<div class='error'><p>Error: $sqlError</p></div>";
+            }
+        }
+    } catch (Throwable $e) {
+        echo "<div class='error'>";
+        echo "<p>Error executing GET request : " . $e->getMessage() . "<p></div>";
+    }
+
+    $budgets = json_decode($response, true);
+    return $budgets;
+}
+
 ?>
--- vue/createPropo.php
+++ vue/createPropo.php
@@ -1,26 +1,41 @@
-
 <div class="pageTitleBar">
-    Créer une Proposition
+    Créer une proposition
 </div>
-<!-- TODO : add for loop to get all themes for a given group to display in a drop down menu isntead of having text input -->
-<!-- TODO : change form action -->
+
 <form class="flex flex-col justify-space-between align-center w-200" method="post" action="<?php echo ROOT_URL.'controleur/createGroup.php'; ?>">
-    <div class="formRow">
-        <label class="flex-1" for="titrePropo">Titre*</label>
-        <input class="flex-2" type="text" name="titrePropo" required>
-    </div>
-    
-    <div class="formRow">
-        <label class="flex-1" for="themesPropo">Thème(s)*</label>
-        <input class="flex-2" type="text" name="themesPropo" required>
-    </div>
 
-    <div class="formRow">
-        <label class="flex-1" for="descPropo">Description*</label>
-        <input class="flex-2" type="text" name="descPropo" required>
-    </div>
+<div class="formRow">
+    <label class="flex-1" for="titreProposition">Titre*</label>
+    <input class="flex-2" type="text" name="titreProposition" required>
+</div>
 
-    <button type="submit">
-        Créer une Proposition ➜
-    </button>
+<div class="formRow">
+    <label class="flex-1" for="descProposition">Description*</label>
+    <input class="flex-2" type="text" name="descProposition" required>
+</div>
+
+<div class="formRow">
+    <label class="flex-1" for="coutProp">Cout*</label>
+    <input class="flex-2" type="number" name="coutProp" required>
+</div>
+
+<div class="formRow">
+    <label class="flex-1" for="idBudget">Budget*</label>
+    <select class="flex-2" name="idBudget" required>
+
+<?php
+
+foreach ($budgets as $budget) {
+    echo "<option value='" . $budget['idBudget'] . ">";
+    echo $budget['nomBudget'] . "</option>";
+}
+
+?>
+    </select>
+</div>
+
+<button type="submit">
+    ➜
+</button>
+
 </form>
